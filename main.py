802e464c2e12990ce59ef7fe6e81303693f5ad1d
velan <srivigneshwaran.velayutham@gmail.com>
2026-04-21 13:58:28 +0530

    done

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+import pandas as pd
+import glob
+import os
+csv_files = glob.glob(os.path.join("C:/Users/Velan/PycharmProjects/quantium", "*.csv"))
+df = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
+df.to_csv("combined_file.csv", index=False)
